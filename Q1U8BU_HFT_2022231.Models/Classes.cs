e59e74cc80d5cf1d3c8c63c70ae11a9f945a360a
KodoD <kodo.dani0@gmail.com>
2022-10-26 21:53:44 +0200

    Model classes

---
+++ Q1U8BU_HFT_2022231.Models/Classes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1U8BU_HFT_2022231.Models
+{
+    internal class Classes
+    {
+    }
+}
--- Q1U8BU_HFT_2022231.Models/Studnets.cs
+++ Q1U8BU_HFT_2022231.Models/Studnets.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace Q1U8BU_HFT_2022231.Models
 {
-    public class Class1
+    public class Students
     {
+
         string con = "[DataLibary]/Diak.mdf;Integrated Security=True";
 
     }
