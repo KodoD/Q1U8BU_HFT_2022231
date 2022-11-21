1764330849a8cbe92552e1a78285950b5775ed32
KodoD <kodo.dani0@gmail.com>
2022-11-21 22:51:36 +0100

    Test starting

--- Q1U8BU_HFT_2022231.Test/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Q1U8BU_HFT_2022231.Test
-{
-    public class Class1
-    {
-    }
-}
---
+++ Q1U8BU_HFT_2022231.Test/Tester.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Q1U8BU_HFT_2022231.Test
+{
+    [TestFixture]
+    public class Tester
+    {
+        [Test]
+        public void CreateTestOne() 
+        {
+        }
+        [Test]
+        public void CreateTestTwo()
+        {
+        }
+        [Test]
+        public void CreateTestThree()
+        {
+        }
+        [Test]
+        public void CreateTestFor()
+        {
+        }
+        [Test]
+        public void DeleteTestOne()
+        {
+        }
+        [Test]
+        public void DeleteTestTwo()
+        {
+        }
+    }
+}
